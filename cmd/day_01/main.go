1972e3f1c7fa1ff47b5b81fcef48aefa2a173434
GaborZeller <gabriel.d.celery@gmail.com>
2025-12-01 16:47:35 +0000

    feat: playing around with zap logger

--- cmd/day_01/main.go
+++ cmd/day_01/main.go
@@ -7,9 +7,14 @@ import (
 
 	"github.com/GabrielDCelery/advent-of-code-2025/internals/day_01"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func main() {
+	logLevel, ok := os.LookupEnv("LOGLEVEL")
+	if !ok {
+		logLevel = "info"
+	}
 	filePath := flag.String("file", "", "input file path")
 	passwordMethod := flag.String("method", "end", "password method")
 	flag.Parse()
@@ -24,7 +29,20 @@ func main() {
 		log.Fatalf("failed to open file at path %s", *filePath)
 	}
 	defer file.Close()
-	logger := zap.NewExample()
+	var atom zap.AtomicLevel
+	switch logLevel {
+	case "debug":
+		atom = zap.NewAtomicLevelAt(zapcore.DebugLevel)
+	default:
+		atom = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	}
+	encoderCfg := zap.NewProductionEncoderConfig()
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderCfg),
+		zapcore.Lock(os.Stdout),
+		atom,
+	))
+	defer logger.Sync()
 	dial, err := day_01.NewDial(*passwordMethod, logger)
 	if err != nil {
 		log.Fatalf("failed to instantiate dial %v", err)
