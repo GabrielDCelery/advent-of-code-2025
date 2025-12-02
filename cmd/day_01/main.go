d3d12e76e184bc08797cafe65b7a7b67fa9916d2
GaborZeller <gabriel.d.celery@gmail.com>
2025-12-02 20:34:44 +0000

    refactor: replaced zaplogger instantiator with custom package for day 1 question

--- cmd/day_01/main.go
+++ cmd/day_01/main.go
@@ -6,48 +6,49 @@ import (
 	"os"
 
 	"github.com/GabrielDCelery/advent-of-code-2025/internals/day_01"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+	"github.com/GabrielDCelery/advent-of-code-2025/internals/logging"
 )
 
 func main() {
 	logLevel, ok := os.LookupEnv("LOGLEVEL")
+
 	if !ok {
 		logLevel = "info"
 	}
+
 	filePath := flag.String("file", "", "input file path")
+
 	passwordMethod := flag.String("method", "end", "password method")
+
 	flag.Parse()
+
 	if *filePath == "" {
 		log.Fatalf("missing param 'file', received %s", *filePath)
 	}
+
 	if *passwordMethod == "" {
 		log.Fatalf("missing param 'method', received %s", *passwordMethod)
 	}
+
 	file, err := os.Open(*filePath)
+
 	if err != nil {
 		log.Fatalf("failed to open file at path %s", *filePath)
 	}
+
 	defer file.Close()
-	var atom zap.AtomicLevel
-	switch logLevel {
-	case "debug":
-		atom = zap.NewAtomicLevelAt(zapcore.DebugLevel)
-	default:
-		atom = zap.NewAtomicLevelAt(zapcore.InfoLevel)
-	}
-	encoderCfg := zap.NewProductionEncoderConfig()
-	logger := zap.New(zapcore.NewCore(
-		zapcore.NewJSONEncoder(encoderCfg),
-		zapcore.Lock(os.Stdout),
-		atom,
-	))
+
+	logger := logging.NewLogger(logLevel)
 	defer logger.Sync()
+
 	dial, err := day_01.NewDial(*passwordMethod, logger)
+
 	if err != nil {
 		log.Fatalf("failed to instantiate dial %v", err)
 	}
+
 	_, err = dial.GetPassword(file)
+
 	if err != nil {
 		log.Fatalf("failed to get password: %v", err)
 	}
