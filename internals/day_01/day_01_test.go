36b52132d4f6f1db26daa35641ad82563ca5ce6b
GaborZeller <gabriel.d.celery@gmail.com>
2025-12-02 21:05:19 +0000

    feat: added zaptest logger to day 1 tests

--- internals/day_01/day_01_test.go
+++ internals/day_01/day_01_test.go
@@ -5,14 +5,18 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest"
 )
 
 func TestDial(t *testing.T) {
 	t.Run("Correctly handles input with 'end' password method", func(t *testing.T) {
 		t.Parallel()
 		// given
+		logger := zaptest.NewLogger(t, zaptest.Level(zapcore.DebugLevel))
+		defer logger.Sync()
 		reader := strings.NewReader("L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82")
-		dial, dialErr := NewDial("end", nil)
+		dial, dialErr := NewDial("end", logger)
 
 		// when
 		result, err := dial.GetPassword(reader)
@@ -26,8 +30,10 @@ func TestDial(t *testing.T) {
 	t.Run("Correctly handles input with 'click' password method", func(t *testing.T) {
 		t.Parallel()
 		// given
+		logger := zaptest.NewLogger(t, zaptest.Level(zapcore.DebugLevel))
+		defer logger.Sync()
 		reader := strings.NewReader("L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82")
-		dial, dialErr := NewDial("click", nil)
+		dial, dialErr := NewDial("click", logger)
 
 		// when
 		result, err := dial.GetPassword(reader)
